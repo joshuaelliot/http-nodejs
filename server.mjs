e1d83acb2378cb845f1911042bf63c63cdeb5160
joshua <85379582+joshuaelliot@users.noreply.github.com>
2023-01-05 12:15:12 -0500

    actuaizando y enviando html

--- server.mjs
+++ server.mjs
@@ -2,5 +2,6 @@ import { createServer } from 'http';
 
 createServer((req, res) => {
   res.write('Hello World!');
+  res.sendFile(__dirname+"/index.html");
   res.end();
 }).listen(process.env.PORT);
